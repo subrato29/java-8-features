dd7c46632c27c2569345da2796b5bc689f120eef
subrato.sarkar <subrato.sarkar@lookout.com>
2022-08-21 20:00:33 -0400

    updated code

--- src/stream/StramFilter_FindAny_OrElse.java
+++ src/stream/StramFilter_FindAny_OrElse.java
@@ -5,31 +5,31 @@ import java.util.List;
 
 public class StramFilter_FindAny_OrElse {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		//list of customer objects
-		List<Customer> customerList = Arrays.asList(
-				new Customer ("Tom", 20),
-				new Customer ("David", 45),
-				new Customer ("Harry", 35),
-				new Customer ("Hero", 60)
-			);
-		
-		//Single condition
-		Customer customer = customerList.stream()
-			.filter(x -> "Harry".equals(x.getName()))
-			.findAny()
-			.orElse(null);
-		
-		System.out.println(customer.getName() + " " + customer.getAge());
-		
-		//Filter with multiple conditions
-		customer = customerList.stream()
-				.filter(x -> "David".equals(x.getName()) && 45 == x.getAge())
-				.findAny()
-				.orElse(null);
-		
-		System.out.println(customer.getName() + " " + customer.getAge());
-	}
+        //list of customer objects
+        List < Customer > customerList = Arrays.asList(
+            new Customer("Tom", 20),
+            new Customer("David", 45),
+            new Customer("Harry", 35),
+            new Customer("Hero", 60)
+        );
+
+        //Single condition
+        Customer customer = customerList.stream()
+            .filter(x -> "Harry".equals(x.getName()))
+            .findAny()
+            .orElse(null);
+
+        System.out.println(customer.getName() + " " + customer.getAge());
+
+        //Filter with multiple conditions
+        customer = customerList.stream()
+            .filter(x -> "David".equals(x.getName()) && 45 == x.getAge())
+            .findAny()
+            .orElse(null);
+
+        System.out.println(customer.getName() + " " + customer.getAge());
+    }
 
 }
--- src/stream/Stram_Filter_Map.java
+++ src/stream/Stram_Filter_Map.java
@@ -6,33 +6,33 @@ import java.util.stream.Collectors;
 
 public class Stram_Filter_Map {
 
-	public static void main(String[] args) {
-		
-		//list of customer objects
-		List<Customer> customerList = Arrays.asList(
-				new Customer ("Tom", 20),
-				new Customer ("David", 45),
-				new Customer ("Harry", 35),
-				new Customer ("Hero", 60)
-			);
-		
-		String name = customerList
-				.stream()
-				.filter(x -> "Harry".equals(x.getName()))
-				.map(Customer:: getName)
-				.findAny()
-				.orElse(null);
-		
-		System.out.println(name);
-		System.out.println("------------------");
-		
-		//Print all name as list
-		List<String> custList = customerList
-										.stream()
-										.map(Customer::getName)
-										.collect (Collectors.toList());
-		
-		custList.forEach(System.out::println);
-	}
+    public static void main(String[] args) {
+
+        //list of customer objects
+        List < Customer > customerList = Arrays.asList(
+            new Customer("Tom", 20),
+            new Customer("David", 45),
+            new Customer("Harry", 35),
+            new Customer("Hero", 60)
+        );
+
+        String name = customerList
+            .stream()
+            .filter(x -> "Harry".equals(x.getName()))
+            .map(Customer::getName)
+            .findAny()
+            .orElse(null);
+
+        System.out.println(name);
+        System.out.println("------------------");
+
+        //Print all name as list
+        List < String > custList = customerList
+            .stream()
+            .map(Customer::getName)
+            .collect(Collectors.toList());
+
+        custList.forEach(System.out::println);
+    }
 
 }
